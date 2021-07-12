da57395fe4043a1add9ea1cee18e76f75e549a3c
Adam Shumann <ashumann13@gmail.com>
2021-07-12 16:58:46 -0500

    Add Discord Support

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const { ScreepsAPI } = require('screeps-api')
 const { DataTypes, Sequelize, Model, Op } = require('sequelize')
 const moment = require('moment')
 const axios = require('axios')
+const events = require('events')
+const Discord = require('discord.js')
 
 const args = minimist(process.argv.slice(2), {
   default: {
@@ -14,6 +16,73 @@ const args = minimist(process.argv.slice(2), {
 
 let api
 const times = {}
+
+const notifiers = [
+  {
+    name: 'discord',
+    /** @type {Discord.Client} */
+    client: null,
+    /** @type {Discord.Channel} */
+    channel: null,
+    async connect () {
+
+      const { discord: { token, channel } = {} } = api.appConfig || {}
+      console.log('Connecting Discord Bot')
+      this.client = new Discord.Client()
+      this.client.login(token)
+      await events.once(this.client, 'ready')
+      this.channel = await this.client.channels.fetch(channel)
+    },
+    disconnect () {
+      if (!this.client) return
+      console.log('Disconnecting Discord Bot')
+      this.client.destroy()
+    },
+    link(url, label) {
+      return `[${label}](${url})`
+    },
+    date(date) {
+      return `<t:${date}>`
+    },
+    async send({ title, text, color, url }) {
+      if (!this.channel) return
+      const message = new Discord.MessageEmbed()
+      .setTitle(title)
+      .setColor(color)
+      .setDescription(text)
+      .setURL(url)
+      await this.channel.send(message)
+    }
+  },
+  {
+    name: 'slack',
+    link(url, label) {
+      return `<${url}|${label}>`
+    },
+    date (date) {
+      const fallback = moment(date * 1000).format()
+      return `<!date^${date}^{date} {time}|${fallback}>`
+    },
+    async send({ title, text, color, url }) {
+      const { slack: { webhook, channel } = {} } = api.appConfig || {}
+      if (!webhook) return
+      await axios.post(webhook, {
+        channel,
+        text: '',
+        attachments: [
+          {
+            fallback: text,
+            text,
+            title,
+            title_link: url,
+            color,
+            ts: Math.floor(Date.now() / 1000)
+          }
+        ]
+      })
+    }
+  }
+]
 
 async function run() {
   const sequelize = new Sequelize(args.db, {
@@ -61,6 +130,11 @@ async function run() {
         }
       }
     })
+    notifiers.forEach(n => {
+      if (!n.connect) n.connect = () => {}
+      if (!n.disconnect) n.disconnect = () => {}
+    })
+    await Promise.all(notifiers.map(n => n.connect()))
     for (const nuke of nukes) {
       const { time, tick } = times[nuke.shard]
       const midway = (nuke.landTime - 25000) < time
@@ -86,49 +160,40 @@ async function run() {
   } catch (err) {
     console.error(`Error processing shards:`, err)
   }
+  await Promise.all(notifiers.map(n => n.disconnect()))
 }
+
 
 async function notify(nuke, type) {
   const eta = nuke.landTime - times[nuke.shard].time
-  const parts = []
   const etaSeconds = Math.floor((eta * times[nuke.shard].tick) / 1000)
   const impact = Math.floor(Math.floor(nuke.landTime / 100) * 100)
   const diff = Math.floor(etaSeconds * 0.05)
   const now = Math.floor(Date.now() / 1000)
   const etaEarly = now + etaSeconds - diff
   const etaLate = now + etaSeconds + diff
-  const etaEarlyText = moment(etaEarly * 1000).format()
-  const etaLateText = moment(etaLate * 1000).format()
   const rcl = nuke.level ? ` (RCL ${nuke.level})` : ''
-  if (process.env.DEBUG) {
-    parts.push('DEBUG MODE')
-  }
-  if (nuke.defender) {
-    parts.push(`Defender: <https://screeps.com/a/#!/profile/${nuke.defender}|${nuke.defender}>`)
-  }
-  parts.push(`Attacker: <https://screeps.com/a/#!/profile/${nuke.attacker}|${nuke.attacker}>`)
-  parts.push(`Launch Site: <https://screeps.com/a/#!/room/${nuke.shard}/${nuke.launchRoomName}|${nuke.shard} - ${nuke.launchRoomName}>`)
-  parts.push(`ETA: ${eta} ticks (between <!date^${etaEarly}^{date} {time}|${etaEarlyText}> to <!date^${etaLate}^{date} {time}|${etaLateText}>)`)
-  parts.push(`History: <https://screeps.com/a/#!/history/${nuke.shard}/${nuke.room}?t=${impact}|tick #${impact}> (Impact time)`)
-  const text = parts.join("\n")
-  console.log(text)
-  const { slack: { webhook, channel } = {} } = api.appConfig || {}
-  if (webhook) {
-    await axios.post(webhook, {
-      channel,
-      text: '',
-      attachments: [
-        {
-          fallback: text,
-          text,
-          title: `${type}: ${nuke.shard} ${nuke.room}${rcl}`,
-          title_link: `https://screeps.com/a/#!/room/${nuke.shard}/${nuke.room}`,
-          color: 'danger',
-          ts: now
-        }
-      ]
+  await Promise.all(notifiers.map(async n => {
+    const parts = []
+    if (process.env.DEBUG) {
+      parts.push('DEBUG MODE')
+    }
+    if (nuke.defender) {
+      parts.push(`Defender: ${n.link(`https://screeps.com/a/#!/profile/${nuke.defender}`, nuke.defender)}`)
+    }
+    parts.push(`Attacker: ${n.link(`https://screeps.com/a/#!/profile/${nuke.attacker}`, nuke.attacker)}`)
+    parts.push(`Launch Site: ${n.link(`https://screeps.com/a/#!/room/${nuke.shard}/${nuke.launchRoomName}`, `${nuke.shard} - ${nuke.launchRoomName}`)}`)
+    parts.push(`ETA: ${eta} ticks (between ${n.date(etaEarly)} to ${n.date(etaLate)})`)
+    parts.push(`History: ${n.link(`https://screeps.com/a/#!/history/${nuke.shard}/${nuke.room}?t=${impact}`, `tick #${impact}`)} (Impact time)`)
+    const text = parts.join("\n")
+    console.log(text)
+    await n.send({
+      title: `${type}: ${nuke.shard} ${nuke.room}${rcl}`,
+      text,
+      url: `https://screeps.com/a/#!/room/${nuke.shard}/${nuke.room}`,
+      color: '#FF0000'
     })
-  }
+  }))
 }
 
 async function getShards(data) {
